65952b8529968c569ba2b9d066de50bcf699f702
mudcrab88 <mudcrab@yandex.ru>
2022-06-13 23:56:09 +0400

    add Mailer

--- manager/src/Model/User/Service/ConfirmTokenSender.php
+++ manager/src/Model/User/Service/ConfirmTokenSender.php
@@ -5,9 +5,31 @@ declare(strict_types=1);
 namespace App\Model\User\Service;
 
 use App\Model\User\Entity\User\Email;
+use Twig\Environment;
 
-interface ConfirmTokenSender
+class ConfirmTokenSender
 {
+    private $mailer;
+    private $twig;
+    private $from;
 
-    public function send(Email $email, string $token): void;
+    public function __construct(\Swift_Mailer $mailer, Environment $twig, array $from)
+    {
+        $this->mailer = $mailer;
+        $this->twig = $twig;
+        $this->from = $from;
+    }
+    public function send(Email $email, string $token): void
+    {
+        $message = (new \Swift_Message('Sig Up Confirmation'))
+            ->setFrom($this->from)
+            ->setTo($email->getValue())
+            ->setBody($this->twig->render('mail/user/signup.html.twig', [
+                'token' => $token
+            ]), 'text/html');
+
+        if (!$this->mailer->send($message)) {
+            throw new \RuntimeException('Unable to send message.');
+        }
+    }
 }
